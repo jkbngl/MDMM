233f92949dbac5ecba96e94802a8064ba980df74
jkbngl <jakob.engl@hotmail.de>
2020-06-06 23:01:00 +0200

    #4 prototype primeNG header bar

--- mdmm/src/app/app.module.ts
+++ mdmm/src/app/app.module.ts
@@ -2,25 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {DropdownModule} from 'primeng/dropdown';
-import {TableModule} from 'primeng/table';
-import { FormsModule } from '@angular/forms';
-import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
 
+import { DropdownModule } from 'primeng/dropdown';
+import { TableModule } from 'primeng/table';
+import { FormsModule } from '@angular/forms';
+import {MenubarModule} from 'primeng/menubar';
 import {MenuItem} from 'primeng/api';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+
+
 import { TableComponent } from './table/table.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';                  //api
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';                  //api
 
 @NgModule({
   declarations: [
     AppComponent,
-    TableComponent
+    TableComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     DropdownModule,
+    MenubarModule,
+    InputTextModule,
+    ButtonModule,
     TableModule,
     FormsModule,
     BrowserAnimationsModule
