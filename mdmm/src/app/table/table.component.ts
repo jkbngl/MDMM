1c7270e4fbbb36107fb0a0210f7acf72543abaea
jkbngl <jakob.engl@hotmail.de>
2020-06-07 19:44:47 +0200

    #4 readded pagination

--- mdmm/src/app/table/table.component.ts
+++ mdmm/src/app/table/table.component.ts
@@ -9,19 +9,37 @@ import { DataService, Book } from '../data.service';
 export class TableComponent implements OnInit {
 
   books: Book[];
+  totalRecords: number;
+  cols: any[];
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-      this.dataService.getData().then(books => this.books = books);
+    this.dataService.getData().then((res) => this.extractData(res));
+    
+
+    this.cols = [
+      { field: 'name', header: 'Name' },
+      {field: 'author', header: 'Author' },
+      { field: 'price', header: 'Price' }      
+    ];
   }
+
+  extractData(books) {       
+    this.books = books;
+    this.totalRecords = this.books.length; 
+  }
+
 
   onRowEditInit(book: Book) {
     console.log('Row edit initialized');
+    console.log(this.books);
   }
 
   onRowEditSave(book: Book) {
     console.log('Row edit saved');
+    console.log(book);
+
   }
 
   onRowEditCancel(book: Book, index: number) {
